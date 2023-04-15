df8675746feaee55caf47af4bb65f1d8a96e6f9d
JoelFrancisco <joelfranciscofilho@gmail.com>
2023-04-15 14:41:24 -0300

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '@components/ @pages/ @images/ @utils/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
